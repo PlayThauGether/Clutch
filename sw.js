2ec009a953d2e544e46dd0d4285e27bb53cceeca
PlayThauGether <playthaugether@gmail.com>
2026-07-05 01:33:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,42 @@
-const CACHE = 'ptg-games-v2';
+const CACHE = 'ptg-games-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './sw.js',
+  './manifest.json',
+  './Clutch_Mot_Logo.png',
+];
 
 self.addEventListener('install', e => {
   self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  // Ne pas intercepter les requêtes Firebase, CDN externes, etc.
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        // Mettre à jour le cache avec la réponse fraîche
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
